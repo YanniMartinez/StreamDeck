3109b0a9f1d4173f57963a9fbef0576f558ccf5d
YanniMartinez <metalyanni@gmail.com>
2023-06-18 13:53:31 -0600

    Agregando mejoras al servidor y cliente para renderizar UI

--- cliente.py
+++ cliente.py
@@ -1,10 +1,49 @@
+import tkinter as tk
 import socket
 
-mi_socket = socket.socket()
-mi_socket.connect(("localhost",8000))
+class ClienteSocketApp:
+    def __init__(self):
+        self.palabras = ["VisualStudio", "PowerPoint", "Excel", "Word"]
+        self.rutas_imagenes = [
+            "visualstudio.png",
+            "powerpoint.png",
+            "excel.png",
+            "word.png"
+        ]
 
-mi_socket.send("Hola desde el cleinte".encode("utf-8"))
-respuesta = mi_socket.recv(1024)
+        self.ventana = tk.Tk()
+        self.ventana.title("Interfaz con Tkinter y Socket")
+        self.ventana.geometry("650x700")
 
-print(respuesta)
-mi_socket.close
+        self.crear_interfaz()
+        self.conectar_servidor()
+
+        self.ventana.mainloop()
+
+    def crear_interfaz(self):
+        for i in range(len(self.palabras)):
+            palabra = self.palabras[i]
+            ruta_imagen = self.rutas_imagenes[i]
+
+            imagen = tk.PhotoImage(file=ruta_imagen)
+            imagen = imagen.subsample(50, 50)
+            boton = tk.Button(self.ventana, text=palabra, image=imagen, compound=tk.TOP,
+                              command=lambda p=palabra: self.boton_presionado(p))
+            boton.image = imagen
+            boton.pack(pady=10)
+
+    def conectar_servidor(self):
+        self.mi_socket = socket.socket()
+        self.mi_socket.connect(("localhost", 8000))
+
+    def boton_presionado(self, palabra):
+        self.mi_socket.send(palabra.encode())
+        respuesta = self.mi_socket.recv(1024)
+        print(respuesta.decode())
+
+    def __del__(self):
+        self.mi_socket.close()
+
+
+if __name__ == "__main__":
+    app = ClienteSocketApp()
--- servidor.py
+++ servidor.py
@@ -1,15 +1,41 @@
 import socket
 
-mi_socket = socket.socket()
-mi_socket.bind (('localhost',8000))
-mi_socket.listen(5)
+class ServidorSocket:
+    def __init__(self):
+        self.mi_socket = socket.socket()
+        self.mi_socket.bind(("localhost", 8000))
+        self.mi_socket.listen(1)
+        print("Servidor iniciado. Esperando conexiones...")
+        self.aceptar_conexion()
 
-while True:
+    def aceptar_conexion(self):
+        self.cliente_socket, self.cliente_direccion = self.mi_socket.accept()
+        print("Cliente conectado:", self.cliente_direccion)
 
-    conexion, addr = mi_socket.accept()
-    print ("Nueva conexion establecida!")
-    print (addr)
-    peticion = conexion.recv(1024)
-    print (peticion)
-    conexion.send("Hola, te saludo desde el servidor!".encode("utf-8"))
-    conexion.close()
+        self.recibir_datos()
+
+    def recibir_datos(self):
+        while True:
+            datos = self.cliente_socket.recv(1024)
+            if not datos:
+                break
+
+            palabra = datos.decode()
+            print("Palabra recibida:", palabra)
+
+            # Aquí puedes agregar la lógica que desees según la palabra recibida
+
+            # Ejemplo: enviar una respuesta al cliente
+            respuesta = "Respuesta a la palabra " + palabra
+            self.cliente_socket.send(respuesta.encode())
+
+        self.cerrar_conexion()
+
+    def cerrar_conexion(self):
+        self.cliente_socket.close()
+        self.mi_socket.close()
+        print("Conexiones cerradas. Servidor detenido.")
+
+
+if __name__ == "__main__":
+    servidor = ServidorSocket()
